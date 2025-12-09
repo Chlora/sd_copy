256a842429bdfe29253f99545cc471ae9a38e1d4
Chlora <158087623+Chlora@users.noreply.github.com>
2025-12-09 20:21:06 +0000

    documentation

--- include/zookeper-interactions.h
+++ include/zookeper-interactions.h
@@ -19,10 +19,22 @@ struct rlist_t;
 extern struct rlist_t *rlistHead;
 extern struct rlist_t *rlistTail;
 
+/**
+ * Establece ligação com o Zookeeper, preenchendo as globais rlistHead e rlistTail, e tanta establecer ligações aos servidores correspondentes.
+ * Retorna 0 em caso de sucesso, ou -1 em caso de erro.
+ */
 int connectToZookeper(char* ip);
 
+/**
+ * Retorna 1 se todas as ligações estiverem establecidas, senão 0.
+ */
 int areConnectionsEstablished();
 
+/**
+ * Termina a ligação com o Zookeeper, assim também como aos servidores correspondentes às globais rlistHead e rlistTail.
+ * Liberta toda a memória associada a estes.
+ * Retorna 0 em caso de sucesso, ou -1 em caso de erro.
+ */
 int disconnectFromZookeeper();
 
 
